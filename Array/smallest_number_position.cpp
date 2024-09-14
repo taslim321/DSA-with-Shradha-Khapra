7cec31a21e240aba13f018b4f4d33a2c31e253ad
Taslim Amin <taslimamin907@gmail.com>
2024-09-14 15:53:31 +0600

    Smallest Element Index

--- Array/smallest_number_position.cpp
+++ Array/smallest_number_position.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<limits.h> // for INT_MAX
 using namespace std;
 
-int main(){
-    int arr[] = {13,27,-04,75,-15};
+int findSmallestNumberIndex(int arr[],int n)
+{
+    int min = INT_MAX;
     
+    for(int i = 0; i<n; i++)
+    {
+        if(min>arr[i])
+        {
+           min = i;
+        }
+    }
+    return min;
+}
+int main(){
+    int arr[] = {13,27,-04,75,-15,38,-40,20};
+
+    int minIndex = findSmallestNumberIndex(arr,8);
+    cout<<minIndex+1<<endl;
+
     return 0;
 }
