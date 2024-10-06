918256872cbed23bff36c52899bb4862029bf609
Taslim Amin <taslimamin907@gmail.com>
2024-10-07 04:12:57 +0600

    Solve Peak index in mountain array

---
+++ Lec-19 Peak Index in Mountain Array/peak_index_in_mountain_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    int st = 1, end = arr.size() - 2;
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+        {
+            return mid;
+        }
+        else if (arr[mid - 1] < arr[mid]) // peak in right side
+        {
+            st = mid + 1;
+        }
+        else // peak in left side
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int>arr = {0,3,8,9,5,4,1};
+    cout<<peakIndexInMountainArray(arr)<<endl;
+    
+    return 0;
+}
